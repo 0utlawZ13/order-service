811a8227e9025e5e32d6573c2991fcd54390d753
Cristian Alemán <bladimiralemanc@gmail.com>
2024-11-12 11:05:49 -0500

    feat: :sparkles: Its the improve of the service at very basic level but response to GET petition

---
+++ order-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, Order Service!")
+	})
+
+	fmt.Println("Starting server on :8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
+	log.Println("Server started on: 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
